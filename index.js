420f88a89c2a0279306bb0574d025e48b75ac929
Elijah Alaak <alaakelijah78@gmail.com>
2024-04-10 19:54:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
     }
     
+    
     function mondayWork(activity = "go to the office") {
     return `This Monday, I will ${activity}.`;
     }
+
     
     function wrapAdjective(wrapper) {
     return function (adjective) {
